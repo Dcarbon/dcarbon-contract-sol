5b5001808e9f08064a56c0588a021a5acfa51459
Chau Khac <chaukhac4@gmail.com>
2024-08-08 23:52:44 +0700

    feat(contract): dcarbon-1207558596855211 listing and buying with token

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,4 +1,4 @@
-import { clusterApiUrl, Connection } from '@solana/web3.js';
+import { clusterApiUrl, Connection, PublicKey } from '@solana/web3.js';
 import { Program } from '@coral-xyz/anchor';
 import { DcarbonContract } from '../target/types/dcarbon_contract';
 import idl from '../target/idl/dcarbon_contract.json';
@@ -29,7 +29,7 @@ const fetchAllTokenFollowProject = async () => {
       {
         memcmp: {
           offset: 8 + 1 + 32 + 8,
-          bytes: bs58.encode(numberToUint8Array(projectId)),
+          bytes: bs58.encode(u16ToBytes(projectId)),
         },
       },
     ],
@@ -44,13 +44,56 @@ const fetchAllTokenFollowProject = async () => {
   }
 };
 
-function numberToUint8Array(num: number) {
-  if (num < 0 || num > 65535) {
-    throw new RangeError('Number must be between 0 and 65535.');
+const fetchAllTokenListingFromProject = async () => {
+  const connection = new Connection(clusterApiUrl('devnet'), 'confirmed');
+
+  const program = new Program<DcarbonContract>(idl as DcarbonContract, {
+    connection: connection,
+  });
+
+  const projectId = 148;
+
+  const accounts = await connection.getProgramAccounts(program.programId, {
+    dataSlice: { offset: 0, length: 0 },
+    filters: [
+      {
+        // dataSize: 8 + 43,
+        memcmp: {
+          offset: 0,
+          bytes: bs58.encode(
+            idl.accounts.find((acc: { name: string; discriminator: number[] }) => acc.name === 'TokenListingInfo')
+              .discriminator as number[],
+          ),
+        },
+      },
+      {
+        memcmp: {
+          offset: 8 + 32 + 32 + 8 + 8,
+          bytes: bs58.encode(u16ToBytes(projectId)),
+        },
+      },
+    ],
+  });
+
+  for (let i = 0; i < accounts.length; i++) {
+    const tokenListingInfoData = await program.account.tokenListingInfo.fetch(accounts[i].pubkey);
+    console.log('Info: ', tokenListingInfoData);
+    const [tokenListingStatus] = PublicKey.findProgramAddressSync(
+      [accounts[i].pubkey.toBuffer(), Buffer.from('token_listing_status')],
+      program.programId,
+    );
+
+    const tokenListingStatusData = await program.account.tokenListingStatus.fetch(tokenListingStatus);
+    console.log('Status: ', tokenListingStatusData);
   }
-  const highByte = (num >> 8) & 0xff; // Extract the high byte
-  const lowByte = num & 0xff; // Extract the low byte
-  return new Uint8Array([highByte, lowByte]);
+};
+fetchAllTokenListingFromProject();
+
+export function u16ToBytes(value: number): Uint8Array {
+  const buffer = new ArrayBuffer(2); // u16 cần 2 bytes
+  const view = new DataView(buffer);
+  view.setUint16(0, value, true); // true để lưu theo Little Endian, false để lưu theo Big Endian
+  return new Uint8Array(buffer);
 }
 
 fetchAllTokenFollowProject();
